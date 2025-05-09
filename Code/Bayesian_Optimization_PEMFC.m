a7188c0974591f2abf6bac3a938100325c8fabbd
plinai <62170175+pengzhulin@users.noreply.github.com>
2025-05-09 15:35:12 +0800

    Update Bayesian_Optimization_PEMFC.m

--- Code/Bayesian_Optimization_PEMFC.m
+++ Code/Bayesian_Optimization_PEMFC.m
@@ -28,9 +28,9 @@ function y = objectiveFunction(x)
     x_NN = predict(NN, [x.x1 x.x2 x.x3]); % get  normalized predicted results from NN
     X_com_1 = x_NN(1); % (1) get normalized porosity predicted by NN
     X_com_2 = 1/x_NN(2); % (1)get normalized tortuosity predicted by NN (2) calculate tortuosity
-    X_com_3 = 10^(x_NN(3)*-15);% (1)get normalized kappa_z predicted by NN (2) calculate kappa_z
-    X_com_4 = 10^(x_NN(4)*-15);% (1)get normalized kappa_y predicted by NN (2) calculate kappa_y
-    X_com_5 = 10^(x_NN(5)*-15);% (1)get normalized kappa_x predicted by NN (2) calculate kappa_x
+    X_com_3 = 10^(x_NN(3)*-15);% 
+    X_com_4 = 10^(x_NN(4)*-15);% 
+    X_com_5 = 10^(x_NN(5)*-15);% 
     %load comsol model
     model = mphopen('folder_path\multiphysical_fuel_cell_comsol_model.mph');
     % set calculated parameters in comsol 
